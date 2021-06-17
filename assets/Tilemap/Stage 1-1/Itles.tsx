afb71d013a77f8ebc032c4ffc3682585180f719a
brunnomenxa <brunnomenxa@gmail.com>
2021-06-17 01:52:36 -0300

    feat: add assets

---
+++ assets/Tilemap/Stage 1-1/Itles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Itles" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="480" columns="20">
+ <image source="Itles.png" trans="000000" width="341" height="410"/>
+</tileset>
